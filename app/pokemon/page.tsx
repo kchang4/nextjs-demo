d9824376e5d834d8208d25abb6d182d19f5524c3
Khai Chang <2335501+kchang4@users.noreply.github.com>
2023-07-28 15:08:36 +0000

    render pokemons

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,5 +1,36 @@
-export default function PokemonList() {
+import { NextPage } from "next"
+
+interface Pokemon {
+  name: string;
+  uri: string;
+}
+
+interface PokemonListProps {
+  pokemons: Pokemon[];
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+  const pokemons = await res.json()
+
+  return {
+    props: {
+      pokemons
+    }
+  }
+}
+
+const PokemonList: NextPage<PokemonListProps> = ({ pokemons }) => {
   return (
-    <div>Pokemon List</div>
+    <div>
+      {pokemons.map((pokemon, index) => {
+        return (
+          <div key={`${pokemon}-${index}`}>
+            <span>{pokemon.name}</span>
+          </div>);
+      })}
+    </div>
   )
 }
+
+export default PokemonList;
